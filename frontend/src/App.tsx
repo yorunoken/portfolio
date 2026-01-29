50cf76cb8702de842b9a0dd1f27615629f0cb6fa
yorunoken <me@yorunoken.com>
2026-01-29 20:34:10 +0300

    gdfgd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import ResolvingTitle from "./components/ResolvingTitle";
+import Footer from "./components/Footer";
+import Quotes from "./components/Quotes";
 
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
-        <main>
-            <h1>Vite + React</h1>
-            <div>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+        <main className="min-h-screen w-full bg-[#111] text-zinc-100 p-6 md:p-10 font-sans antialiased selection:bg-blue-500/30">
+            <div className="max-w-4xl mx-auto space-y-18">
+                <header className="space-y-2 mt-6 md:mt-12">
+                    <div className="space-y-4">
+                        <ResolvingTitle text="yorunoken" className="text-4xl font-bold tracking-tight text-white mb-2" />
+                        <p className="text-lg text-zinc-400 font-mono">Software Engineer & Full Stack Developer</p>
+                    </div>
+
+                    <Quotes />
+                </header>
+
+                <About />
+
+                <Projects />
+
+                <Footer />
             </div>
-            <p>Click on the Vite and React logos to learn more</p>
         </main>
     );
 }
-
-export default App;
